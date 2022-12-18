d91e95f6873351f18dc1c122c614cbaea8000026
山下隼樹 <junki@yamashitashunjunoMacBook-Pro.local>
2022-12-18 16:41:55 +0900

    #3 ログイン用ヘッダーコンポーネントを全ページに適用

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { UserProvider } from "@auth0/nextjs-auth0/client";
+import Header from "../components/Header";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }: AppProps) => {
+  const { user } = pageProps;
+  return (
+    <UserProvider user={user}>
+      <Header />
+
+      <Component {...pageProps} />
+    </UserProvider>
+  );
+};
+export default App;
